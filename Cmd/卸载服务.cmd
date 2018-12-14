1a77424f07856b2b4f5058d0a35c979fa9fee792
李帅 <1028789852@qq.com>
2018-12-14 09:43:41 +0800

    submit

---
+++ Cmd/卸载服务.cmd
@@ -0,0 +1,13 @@
+@echo off
+color 0A
+echo -----------------------------------------------------  
+echo 当前执行 - 安装服务  
+echo 当前路径 - %~f0 
+echo 当前时间 - %date% %time% 
+set a=%~f0 
+set b=%a:~0,-13%
+set c=bin\debug\WP.Schedule.exe uninstall
+set d=%b%%c%
+%d% 
+echo -----------------------------------------------------   
+cmd /k
---
+++ Cmd/卸载服务.ps1
@@ -0,0 +1,10 @@
+echo -----------------------------------------------------  
+echo 当前执行-安装服务  
+$x = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$a = $x.Substring(0,$x.Length-3)
+$b = "bin\debug\WP.Schedule.exe uninstall"
+$c = $a + $b
+powershell.exe -Command $c
+echo -----------------------------------------------------  
+pause
+ 
